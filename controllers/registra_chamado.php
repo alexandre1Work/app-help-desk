e712c3706a98be201b45181e050fb19f6b307af8
alexandre1Work <alexandre.velosowork@gmail.com>
2025-04-12 15:34:41 -0300

    Refactor: refactoring the project structure

--- controllers/registra_chamado.php
+++ controllers/registra_chamado.php
@@ -1,21 +1,6 @@
 <?php
-    //abre e cria o arquivo caso não exista
-    $arquivo = fopen('../storage/chamados.hd', 'a');
+    require_once '../models/ChamadosModel.php';
 
-    //trata os dados do formulario
-    $titulo = str_replace('#', '-', $_POST['titulo']); 
-    $categoria = str_replace('#', '-', $_POST['categoria']);
-    $descricao = str_replace('#', '-', $_POST['descricao']);
-
-    //armazena o conteudo do formulário
-    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
-
-    //ecreve o conteudo no arquivo
-    //1p abre o arq 2p escreve no arq 3a adiciona o conteudo no arq
-    fwrite($arquivo, $texto);
-
-    //fecha o arquivo
-    fclose($arquivo);
-
-    header('Location: ../pages/home.php');
-?>
+    ChamadoModel::salvar($_POST['titulo'], $_POST['categoria'], $_POST['descricao']);
+    header('Location: ../views/sucesso.php');
+    exit;
---
+++ models/chamadosModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class ChamadoModel
+{
+    public static function salvar($titulo, $categoria, $descricao)
+    {
+        //trata os dados do formulario
+        $titulo = str_replace('#', '-', $_POST['titulo']);
+        $categoria = str_replace('#', '-', $_POST['categoria']);
+        $descricao = str_replace('#', '-', $_POST['descricao']);
+
+        //armazena o conteudo do formulário
+        $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+        
+        $arquivo = fopen('../storage/chamados.hd', 'a'); //abre e cria o arquivo caso não exista
+        fwrite($arquivo, $texto); //ecreve o conteudo no arquivo
+        fclose($arquivo); //fecha o arquivo         
+
+        return true;
+    }
+}
