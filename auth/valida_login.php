d12a0f77a376f405441703cab418b43459aa9ef0
alexandre1Work <alexandre.velosowork@gmail.com>
2025-04-14 15:15:51 -0300

    Feat: user profile control

--- auth/valida_login.php
+++ auth/valida_login.php
@@ -4,18 +4,31 @@ session_start();
 
 //váriavel que verifica se a autenticação foi realizada
 $usuário_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
 
-$usuarios_app[] = array('email' => 'teste@gmail.com', 'senha' => '123456');
+$perfis = array(1 => 'administrativo', 2 => 'usuario');
+
+$usuarios_app = array(
+    array('id' => 1, 'email' => 'adm@gmail.com', 'senha' => '123456', 'perfil_id' => 1),
+    array('id' => 2, 'email' => 'user@gmail.com', 'senha' => '123456', 'perfil_id' => 1),
+    array('id' => 3, 'email' => 'jose@gmail.com', 'senha' => '123456', 'perfil_id' => 2),
+    array('id' => 4, 'email' => 'maria@gmail.com', 'senha' => '123456', 'perfil_id' => 2),
+);
 
 foreach($usuarios_app as $user) {
     if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
         $usuário_autenticado = true;
+        $usuario_id = $user['id'];
+        $usuario_perfil_id = $user['perfil_id'];
         break;
     } 
 }
 
 if($usuário_autenticado) {
     $_SESSION['autenticado'] = 'SIM';
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['perfil_id'] = $user['perfil_id'];
     header('Location: ../views/home.php');
 } else {
     $_SESSION['autenticado'] = 'NÃO';
--- models/chamadosModel.php
+++ models/chamadosModel.php
@@ -1,16 +1,18 @@
 <?php
-
+    session_start();
 class ChamadoModel
 {
+
     public static function salvar($titulo, $categoria, $descricao)
     {
+
         //trata os dados do formulario
         $titulo = str_replace('#', '-', $_POST['titulo']);
         $categoria = str_replace('#', '-', $_POST['categoria']);
         $descricao = str_replace('#', '-', $_POST['descricao']);
 
         //armazena o conteudo do formulário
-        $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+        $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
         
         $arquivo = fopen('../storage/chamados.hd', 'a'); //abre e cria o arquivo caso não exista
         fwrite($arquivo, $texto); //ecreve o conteudo no arquivo
